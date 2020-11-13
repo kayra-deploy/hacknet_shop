78023816bc9d6777c5a2020d6ecaf96a281ce552
kayra-deploy <74431843+kayra-deploy@users.noreply.github.com>
2020-11-14 02:41:08 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python
-import telebot,random
-from qiwi import *
+import telebot,random,requests
+#=========================
+login = '79125946990'
+token1 = '4d6d411bbc2ef32553be668ac1494f9e'
+
+def last_pay(my_login, token, rows_num, next_TxnId, next_TxnDate):
+    s = requests.Session()
+    s.headers['authorization'] = 'Bearer ' + token  
+    parameters = {'rows': rows_num, 'nextTxnId': next_TxnId, 'nextTxnDate': next_TxnDate}
+    h = s.get('https://edge.qiwi.com/payment-history/v2/persons/' + my_login + '/payments', params = parameters)
+    return h.json()
+
+def get_last_pay():
+    a = last_pay(login, token1, '1', '', '')
+    return a['data'][0]['status'], a['data'][0]['sum']['amount'], a['data'][0]['comment']
+#==========================
 token = '1347566794:AAE9L1SBoTGT8peiqxjwb4yyUugr1MsLauQ'
 bot = telebot.TeleBot(token)
 
